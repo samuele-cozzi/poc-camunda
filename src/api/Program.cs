7c3d1c228d139a7f7a452f082461c16b2894d514
samuele-cozzi <samuele.cozzi@gmail.com>
2023-10-22 21:20:34 +0200

    helm & scaffold

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHealthChecks();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapHealthChecks("/");
+
+app.MapGet("/hello", () => "Hello World!");
 
 app.Run();
